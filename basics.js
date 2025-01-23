49cd8a418d213ce7748c3322fb8e3197f8209eda
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2025-01-23 09:17:47 +0300

    JavaScript Exercises

---
+++ basics.js
@@ -0,0 +1,24 @@
+var numOne = 6
+var numTwo = 11
+
+var summ = numOne+ numTwo
+var division = numOne/numTwo
+var divv = Math.trunc(numTwo/numOne)
+var remainder = numTwo%numOne
+
+
+console.log(summ)
+console.log(division)
+console.log(divv)
+console.log(remainder)
+
+let firstName = "Rachel"
+
+let welcome = `Welcome back ${firstName}` //string 
+
+console.log("Welcome back " + " " + firstName)
+
+// console.log(welcome)
+
+console.log(firstName.toLocaleUpperCase)
+console.log(firstName.length)
