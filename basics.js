e41c62576a2c05598006d78470071e9aefd38025
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2025-01-23 09:38:01 +0300

    Update

--- basics.js
+++ basics.js
@@ -28,3 +28,27 @@ let isHall = true
 console.log(isHall && isStudent)
 console.log(isHall || isStudent)
 
+let fruits = ["mangoes", "oranges", "grapes"]
+console.log(fruits)
+fruits.push("apples")
+console.log(fruits)
+fruits.pop()
+// fruits.del("oranges")
+console.log(fruits)
+let myFruits = fruits.splice(1,1)
+console.log(myFruits)
+
+
+let signup = {
+    "name" : "Rachel",
+    "surname": "Isooba",
+    "age":24,
+    "country": "Uganda",
+    "inClass": false,
+    "phone": "0707125552",
+    "email": "mbeizacokila@gmail.com",
+    "password":"BeMyself2024"
+
+}
+
+console.log(signup)
